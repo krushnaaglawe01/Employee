d52886a56c50933f7bae768e7ca6032bcf1dc305
krushnaaglawe01 <krushna.aglawe01@gmail.com>
2026-04-21 17:10:29 +0530

    second commit

---
+++ Employee/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -0,0 +1,55 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Employee;
+import com.example.demo.service.EmployeeService;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+@RestController
+
+public class EmployeeController {
+
+	@Autowired
+	private EmployeeService es;
+	
+	@GetMapping("/")
+	public String Home() {
+		return "Hello World";
+	}
+	
+	@PostMapping("add")
+	public void add(@RequestBody Employee emp) {
+		//TODO: process POST request
+		
+		es.add(emp);
+	}
+	
+	@GetMapping("display")
+	public List<Employee> display() {
+		return es.display();
+	}
+	
+	@PutMapping("update/{id}")
+	public Employee update(@PathVariable Integer id, @RequestBody Employee emp) {
+		//TODO: process PUT request
+		
+		return es.update(emp, id);
+	}
+	
+	@DeleteMapping("delete/{id")
+	public void delete(@PathVariable Integer id) {
+		es.delete(id);
+	}
+}
+ 
---
+++ Employee/src/main/java/com/example/demo/service/EmployeeServiceImpl.java
@@ -0,0 +1,41 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Employee;
+import com.example.demo.repository.EmployeeRepository;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+
+	@Autowired
+	private EmployeeRepository empRepo;
+	
+	@Override
+	public void add(Employee emp) {
+		// TODO Auto-generated method stub
+		
+		empRepo.save(emp);
+	}
+
+	@Override
+	public List<Employee> display() {
+		// TODO Auto-generated method stub
+		return empRepo.findAll();
+	}
+	
+	@Override
+	public Employee update(Employee emp, Integer id) {
+		emp.setId(id);
+		return empRepo.save(emp);
+	}
+	
+	@Override
+	public void delete(Integer id) {
+		empRepo.deleteById(id);
+	}
+
+}
